cff8dc13f8f7684009f68098cdb27c13dd4009fe
y3km21 <y3km21@gmail.com>
2022-04-26 20:10:22 +0900

    Settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    outputStandalone: true,
+  },
 
-module.exports = nextConfig
+  async headers() {
+    return customHeaders;
+  },
+
+  webpack: (config, options) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    return config;
+  },
+};
+
+let customHeaders = [
+  {
+    source: "/",
+    headers: [
+      // https://web.dev/coop-coep/
+      // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer
+      { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+      { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+
+      { key: "Access-Control-Allow-Origin", value: "*" },
+      { key: "Cross-Origin-Resource-Policy", value: "cross-origin" },
+    ],
+  },
+
+  {
+    source: "/:slug*/:slug([^/]+\\.js|[^/]+\\.html)",
+    headers: [
+      { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+      { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+    ],
+  },
+];
+
+module.exports = nextConfig;
---
+++ typed-scss-modules.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  watch: true,
+  implementation: "sass",
+  nameFormat: "none",
+  exportType: "default",
+  exportTypeName: "TheClasses",
+  aliasPrefixes: { "@": "/" },
+  logLevel: "verbose",
+  banner: "// default",
+};
